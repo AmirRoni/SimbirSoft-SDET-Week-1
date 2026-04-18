1710f76013705fa49152d0cc390622bdc58cba2e
Amir <ronigats@mail.ru>
2026-04-18 23:21:08 +0300

    Add SearchPage and the initial successful test

--- tests/test_search_and_cart.py
+++ tests/test_search_and_cart.py
@@ -1,10 +1,21 @@
+import random
+
 from pages.home_page import HomePage
+from pages.search_page import SearchPage
 
 
 def test_search_and_cart(driver):
     home_page = HomePage(driver)
+    search_page = SearchPage(driver)
 
     home_page.open()
     home_page.search("shirt")
+
+    search_page.sort_by_name_asc()
+
+    for product_index in [1, 2]:
+        search_page.open_product_by_index(product_index)
+
+        quantity = random.randint(1, 3)
 
     assert "Search"
